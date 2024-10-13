46d66ec8759fe3fc9bbe611a02ae82257a4221ca
Joe Balzac <joebalzac@Mac.lan>
2024-10-12 22:53:51 -0400

    adds chakra ui commit

--- game-hub/src/main.tsx
+++ game-hub/src/main.tsx
@@ -8,7 +8,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
-      <App />
     </ChakraProvider>
   </React.StrictMode>
 );
