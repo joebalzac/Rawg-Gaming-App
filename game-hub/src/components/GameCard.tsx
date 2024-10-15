e96dd87c7597efce960c6064109bf5d04493bed1
Joe Balzac <jgbalzac@gmail.com>
2024-10-15 16:39:28 -0400

    Reformat layout

--- game-hub/src/components/GameCard.tsx
+++ game-hub/src/components/GameCard.tsx
@@ -11,8 +11,8 @@ const GameCard = ({ game }: Props) => {
     <Card borderRadius="10px" overflow="hidden">
       <Image src={game.background_image} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
         <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
