e381aeec4b07ffd757c222bf95249047d963fd03
Joe Balzac <jgbalzac@gmail.com>
2024-10-17 15:52:26 -0400

    adds loading skeletons

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -24,24 +24,27 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const constroller = new AbortController();
-
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", { signal: constroller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
-      })
-      .finally(() => setLoading(false));
+        setLoading(false);
+      });
 
     return () => constroller.abort();
   }, []);
 
-  return { games, error, loading };
+  return { games, error, isLoading };
 };
 
 export default useGames;
